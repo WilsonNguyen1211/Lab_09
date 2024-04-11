f063ec82e4bc295b38fb527435ce92c0dad47492
BlakeStoltz <blakestoltz@icloud.com>
2024-04-10 21:49:29 -0400

    decoder

--- WN.py
+++ WN.py
@@ -7,31 +7,38 @@ def encode(password):
     return encoded_password
 
 
-
+# bds decoder
 def decode(encoded_password):
-    pass
+        decoded_password = ''
+        for num in encoded_password:
+            decoded_number = str((int(num) + 2) % 10)
+            decoded_password += decoded_number
+        return decoded_password
 
+def main():
+        while True:
+            print("Menu")
+            print("-------------")
+            print("1. Encode\n"
+                  "2. Decode\n"
+                  "3. Quit")
+            choice = int(input("Please enter an option: "))
+            password_input = input("Please enter your password to encode: ")
+            if password_input.isnumeric():
+                print("Your password has been encoded and stored!\n")
+            else:
+                print("Invalid password, please try again.")
+            if choice == 1:
+                encoded_password = encode(password_input)
+                print(encoded_password)
+            elif choice == 2:
+                decoded_password = decode(encoded_password)
+                print(f"The encoded password is {encoded_password}, and original password is {decoded_password}.")
+            else:
+                break
 
-if __name__ == '__main__':
-    while True:
-        print("Menu")
-        print("-------------")
-        print("1. Encode\n"
-              "2. Decode\n"
-              "3. Quit")
-        choice = int(input("Please enter an option: "))
-        password_input = input("Please enter your password to encode: ")
-        if password_input.isnumeric():
-            print("Your password has been encoded and stored!\n")
-        else:
-            print("Invalid password, please try again.")
-        if choice == 1:
-            encoded_password = encode(password_input)
-            print(encoded_password)
-        elif choice == 2:
-            decoded_password = decode(encoded_password)
-            print(f"The encoded password is {encoded_password}, and original password is {decoded_password}.")
-        else:
-            break
+if __name__ == "__main__":
+    main()
+
 
 
