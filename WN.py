c73f668459e603f6e5c4f249de748c3b6a4eb712
WilsonNguyen1211 <w.nguyen@ufl.edu>
2024-04-10 21:21:38 -0400

    Lab file

---
+++ WN.py
@@ -0,0 +1,36 @@
+def encode(password):
+    encoded_password = ''
+    for num in password:
+        if num.isdigit():
+            encoded_number = str((int(num) + 8) % 2)
+            encoded_password += encoded_number
+    return encoded_password
+
+
+
+def decode(encoded_password):
+    pass
+
+
+if __name__ == '__main__':
+    while True:
+        print("Menu")
+        print("-------------")
+        print("1. Encode\n"
+              "2. Decode\n"
+              "3. Quit")
+        choice = int(input("Please enter an option: "))
+        password_input = input("Please enter your password to encode: ")
+        if password_input.isnumeric():
+            print("Your password has been encoded and stored!\n")
+        else:
+            print("Invalid password, please try again.")
+        if choice == 1:
+            encoded_password = encode(password_input)
+            print(encoded_password)
+        elif choice == 2:
+            decoded_password = decode(encoded_password)
+            print(f"The encoded password is {encoded_password}, and original password is {decoded_password}.")
+        else:
+            break
+
